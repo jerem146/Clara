d229827901e4329740291d0b4ab72c21929aa1f4
https_(S2) <johanstil028@gmail.com>
2025-05-28 11:41:13 -0500

    Update grupo-config.js

--- plugins/grupo-config.js
+++ plugins/grupo-config.js
@@ -3,13 +3,9 @@ const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => icono)
 let isClose = { // Switch Case Like :v
 'open': 'not_announcement',
 'close': 'announcement',
-'abierto': 'not_announcement',
-'cerrado': 'announcement',
-'abrir': 'not_announcement',
-'cerrar': 'announcement',
 }[(args[0] || '')]
 if (isClose === undefined)
-return conn.reply(m.chat, `${emoji} *Elija una opción para configurar el grupo*\n\nEjemplo:\n*✰ #${command} abrir*\n*✰ #${command} cerrar*\n*✰ #${command} close*\n*✰ #${command} open*`, m)
+return conn.reply(m.chat, `${emoji} *Elija una opción para configurar el grupo*\n\nEjemplo:\n*✰ #open*\n*✰ #close*`, m)
 await conn.groupSettingUpdate(m.chat, isClose)
 
 if (isClose === 'not_announcement'){
@@ -19,9 +15,9 @@ m.reply(`${emoji} *Ya pueden escribir en este grupo.*`)
 if (isClose === 'announcement'){
 m.reply(`${emoji2} *Solos los admins pueden escribir en este grupo.*`)
 }}
-handler.help = ['group open / close', 'grupo abrir / cerrar']
+handler.help = ['open', 'close']
 handler.tags = ['grupo']
-handler.command = ['group', 'grupo',]
+handler.command = ['open', 'close']
 handler.admin = true
 
 export default handler
