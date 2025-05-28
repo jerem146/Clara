598a1b29b3e286dae68eaeb2e081fc7967c1bbf6
https_(S2) <johanstil028@gmail.com>
2025-05-28 11:44:14 -0500

    Update grupo-config.js

--- plugins/grupo-config.js
+++ plugins/grupo-config.js
@@ -1,23 +1,29 @@
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => icono) 
-let isClose = { // Switch Case Like :v
-'open': 'not_announcement',
-'close': 'announcement',
-}[(args[0] || '')]
-if (isClose === undefined)
-return conn.reply(m.chat, `${emoji} *Elija una opción para configurar el grupo*\n\nEjemplo:\n*✰ #open*\n*✰ #close*`, m)
-await conn.groupSettingUpdate(m.chat, isClose)
+let handler = async (m, { conn, args, command }) => {
+  const isClose = {
+    'open': 'not_announcement',
+    'close': 'announcement',
+  }[(args[0] || '')];
 
-if (isClose === 'not_announcement'){
-m.reply(`${emoji} *Ya pueden escribir en este grupo.*`)
-}
+  if (isClose === undefined) {
+    return conn.reply(m.chat, `*Uso:*\n*#open / #close*`, m);
+  }
 
-if (isClose === 'announcement'){
-m.reply(`${emoji2} *Solos los admins pueden escribir en este grupo.*`)
-}}
-handler.help = ['open', 'close']
-handler.tags = ['grupo']
-handler.command = ['open', 'close']
-handler.admin = true
+  try {
+    await conn.groupSettingUpdate(m.chat, isClose);
+    if (isClose === 'not_announcement') {
+      m.reply(`*El grupo está abierto para todos los miembros.*`);
+    } else {
+      m.reply(`*El grupo está cerrado. Solo los admins pueden escribir.*`);
+    }
+  } catch (error) {
+    console.error(error);
+    m.reply(`Hubo un error al cambiar la configuración del grupo.`);
+  }
+};
 
-export default handler
+handler.help = ['open', 'close'];
+handler.tags = ['grupo'];
+handler.command = ['open', 'close'];
+handler.admin = true;
+
+export default handler;
