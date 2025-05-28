67bf784685976292af58f986817cf78891424964
https_(S2) <johanstil028@gmail.com>
2025-05-28 11:35:16 -0500

    Update grupo-config.js

--- plugins/grupo-config.js
+++ plugins/grupo-config.js
@@ -21,7 +21,7 @@ m.reply(`${emoji2} *Solos los admins pueden escribir en este grupo.*`)
 }}
 handler.help = ['group open / close', 'grupo abrir / cerrar']
 handler.tags = ['grupo']
-handler.command = ['group', 'grupo', 'open', 'close', 'abrir', 'cerrar']
+handler.command = ['group', 'grupo',]
 handler.admin = true
 
 export default handler
