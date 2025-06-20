2bb972f44a7ab8bf875c4bbab7982fbd2ff26547
jerem146 <vasquezever146@gmail.com>
2025-06-19 22:17:09 -0500

    Actualizar main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -30,15 +30,15 @@ console.log(`Error : ${e}`)
 global.creador = 'Wa.me/573154062343'
 global.ofcbot = `${conn.user.jid.split('@')[0]}`
 global.asistencia = 'Wa.me/573154062343'
-global.namechannel = '=͟͟͞❀ Destiny - 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 ⏤͟͟͞͞★'
-global.namechannel2 = '=͟͟͞❀ Destiny - 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 ⏤͟͟͞͞★'
-global.namegrupo = 'ᰔᩚ Destiny • ᥆𝖿іᥴіᥲᥣ ❀'
-global.namecomu = 'ᰔᩚ Destiny • ᥴ᥆mᥙᥒі𝗍ᥡ ❀'
+global.namechannel = ''
+global.namechannel2 = ''
+global.namegrupo = ''
+global.namecomu = ''
 global.listo = '❀ *Aquí tienes ฅ^•ﻌ•^ฅ*'
 global.fotoperfil = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://files.catbox.moe/xr2m6u.jpg')
 
 global.canalIdM = ["120363399504708103@newsletter", "120363399504708103@newsletter"]
-global.canalNombreM = ["ᰔᩚ Destiny-mძ • ᥙ⍴ძᥲ𝗍ᥱs ❀", "ᰔᩚ Destiny-mძ • ᥲ᥎іs᥆s ❀"]
+global.canalNombreM = ["", ""]
 global.channelRD = await getRandomChannel()
 
 global.d = new Date(new Date + 3600000)
