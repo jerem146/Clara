988317734a81503ce89923da2467ee31f49abb29
jerem146 <vasquezever146@gmail.com>
2025-06-20 08:50:40 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -212,7 +212,7 @@ if (opcion == '1' || methodCodeQR) {
 console.log(chalk.bold.yellow(`\n❐ ESCANEA EL CÓDIGO QR EXPIRA EN 45 SEGUNDOS`))}
 }
 if (connection == 'open') {
-console.log(chalk.bold.green('\n❀ ShirokoBot- Conectada con éxito ❀'))
+console.log(chalk.bold.green('\n❀ Clara- Conectada con éxito ❀'))
 }
 let reason = new Boom(lastDisconnect?.error)?.output?.statusCode
 if (connection === 'close') {
